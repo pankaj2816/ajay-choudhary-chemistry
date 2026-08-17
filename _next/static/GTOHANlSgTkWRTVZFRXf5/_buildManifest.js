6886f97fa5a1cc41ed829d251b453c3861565163
pankaj2816 <pppankaj2816@gmail.com>
2026-08-17 17:11:15 +0000

    Deploying to gh-pages from @ pankaj2816/ajay-choudhary-chemistry@1b39d14904661e6a5580608bec3e987932572815 🚀

---
+++ _next/static/GTOHANlSgTkWRTVZFRXf5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ajay-choudhary-chemistry//_next/:path+",
+        "destination": "/ajay-choudhary-chemistry/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
